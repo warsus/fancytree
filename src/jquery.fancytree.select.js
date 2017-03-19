5c6cc8b9f8109793c5f48ee62bcd3971764eb9eb
Martin@MBP <github@wwwendt.de>
2017-03-19 13:42:03 +0100

    Cleanup

--- src/jquery.fancytree.select.js
+++ src/jquery.fancytree.select.js
@@ -1,7 +1,7 @@
 /*!
  * jquery.fancytree.select.js
  *
- * Configurable support for hierarcical selection.
+ * Configurable support for hierarchical selection.
  * (Extension module for jquery.fancytree.js: https://github.com/mar10/fancytree/)
  *
  * Copyright (c) 2008-2017, Martin Wendt (http://wwWendt.de)
@@ -33,8 +33,7 @@ $.ui.fancytree.registerExtension({
 	version: "@VERSION",
 	// Default options for this extension.
 	options: {
-		checkboxIcon: true,   // bool | "radio" | function
-		mode: "dimm"       // Grayout unmatched nodes (pass "hide" to remove unmatched node instead)
+		checkboxIcon: true   // bool | "radio" | function
 	},
 	treeInit: function(ctx){
 		// gridnav requires the table extension to be loaded before itself
@@ -45,48 +44,11 @@ $.ui.fancytree.registerExtension({
 	},
 	nodeLoadChildren: function(ctx, source) {
 		return this._superApply(arguments).done(function() {
-			if( ctx.tree.enableFilter && ctx.tree.lastFilterArgs && ctx.options.filter.autoApply ) {
-				ctx.tree._applyFilterImpl.apply(ctx.tree, ctx.tree.lastFilterArgs);
-			}
+			// TODO
 		});
 	},
-	nodeSetExpanded: function(ctx, flag, callOpts) {
-		delete ctx.node._filterAutoExpanded;
-		// Make sure counter badge is displayed again, when node is beeing collapsed
-		if( !flag && ctx.options.filter.hideExpandedCounter && ctx.node.$subMatchBadge ) {
-			ctx.node.$subMatchBadge.show();
-		}
+	nodeSetSelected: function(ctx, flag) {
 		return this._superApply(arguments);
-	},
-	nodeRenderStatus: function(ctx) {
-		// Set classes for current status
-		var res,
-			node = ctx.node,
-			tree = ctx.tree,
-			opts = ctx.options.filter,
-			$title = $(node.span).find("span.fancytree-title"),
-			$span = $(node[tree.statusClassPropName]);
-
-		res = this._super(ctx);
-		// nothing to do, if node was not yet rendered
-		if( !$span.length || !tree.enableFilter ) {
-			return res;
-		}
-		$span
-			.toggleClass("fancytree-match", !!node.match)
-			.toggleClass("fancytree-submatch", !!node.subMatchCount)
-			.toggleClass("fancytree-hide", !(node.match || node.subMatchCount));
-		// Add/update counter badge
-		if( opts.counter && node.subMatchCount && (!node.isExpanded() || !opts.hideExpandedCounter) ) {
-			if( !node.$subMatchBadge ) {
-				node.$subMatchBadge = $("<span class='fancytree-childcounter'/>");
-				$("span.fancytree-icon, span.fancytree-custom-icon", node.span).append(node.$subMatchBadge);
-			}
-			node.$subMatchBadge.show().text(node.subMatchCount);
-		} else if ( node.$subMatchBadge ) {
-			node.$subMatchBadge.hide();
-		}
-			return res;
 	}
 });
 }(jQuery, window, document));
